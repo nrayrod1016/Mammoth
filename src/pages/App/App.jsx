1cb41d2c760a0df5902b1b05164708ce8f43b501
Blake R <blakeromano19@gmail.com>
2021-08-13 09:43:50 -0400

    hooks

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Redirect } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 import Signup from "../Signup/Signup";
@@ -7,34 +7,34 @@ import * as authService from '../../services/authService'
 import * as profileAPI from '../../services/profileService'
 import "./App.css";
 
-class App extends Component {
-  state = {
-    user: authService.getUser(),
-    userProfile: null,
-  }
+const App = ({ history }) => {
+  const [user, setUser] = useState(authService.getUser())
+  const [userProfile, setUserProfile] = useState(null)
   
-  handleLogout = () => {
+  const handleLogout = () => {
     authService.logout();
-    this.setState({ user: null, userProfile: null });
-    this.props.history.push("/");
-  };
-
-  handleSignupOrLogin = async() => {
-    this.setState({ user: await authService.getUser(), userProfile: await profileAPI.getUserProfile() });
-  };
-
-  async componentDidMount() {
-    if (!this.state.userProfile) {
-      const userProfile = await profileAPI.getUserProfile()
-      this.setState({userProfile})
-    }
+    setUser(null);
+    setUserProfile(null)
+    history.push("/");
   }
 
-  render() {
-    const { user, userProfile } = this.state
-    return (
-      <>
-        <NavBar user={user} handleLogout={this.handleLogout} />
+  const handleSignupOrLogin = async() => {
+    const user = await authService.getUser()
+    setUser(user)
+    const userProfile = await profileAPI.getUserProfile()
+    setUserProfile(userProfile)
+  };
+
+  useEffect(() => {
+      profileAPI.getUserProfile()
+      .then(userProfile => {
+        console.log(userProfile)
+        setUserProfile(userProfile)
+      })
+  }, [user])
+  return (
+          <>
+        <NavBar user={user} handleLogout={handleLogout} />
         <Route
           exact
           path="/"
@@ -50,7 +50,7 @@ class App extends Component {
           render={({ history }) => (
             <Signup
               history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
+              handleSignupOrLogin={handleSignupOrLogin}
             />
           )}
         />
@@ -60,13 +60,12 @@ class App extends Component {
           render={({ history }) => (
             <Login
               history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
+              handleSignupOrLogin={handleSignupOrLogin}
             />
           )}
         />
       </>
-    );
-  }
+  );
 }
-
+ 
 export default App;
