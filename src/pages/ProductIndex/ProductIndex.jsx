93c2dee63599a2873dbe3e7e10729cdb97ac35a8
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-08-14 15:15:08 -0400

    products page works

--- src/pages/ProductIndex/ProductIndex.jsx
+++ src/pages/ProductIndex/ProductIndex.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import ProductCard from '../../components/ProductCard/ProductCard'
+import  * as productService from '../../services/productService'
 
+const ProductIndex = (props) => {
+  const [products, setProducts] = useState([{}])
 
-const ProductIndex = () => {
+  useEffect(() => { 
+    productService.getAll() 
+    .then(products => { 
+     setProducts(products)
+    })  
+
+  }, []) 
+  
   return (
     <>
+    <h1> Product List </h1> 
+
+    {products.map(product => {
+      return (
+        <ProductCard Product={product} key={product._id}/> 
+      )
+    }
+    )}
+
+
     </>
   );
 }
