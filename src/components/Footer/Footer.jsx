026c6776059fcb07ff930c1d33562898f642a8c7
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-08-14 00:45:36 -0400

    test2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 // import styles from './NavBar.module.css'
 // import NavBar from './index.css'
 
-const Footer = ({ user, handleLogout }) => {
+const Footer = () => {
     return (
 <>
     <main> 
-        
+
     </main> 
 </>
   )
