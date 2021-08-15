b624011c74cf8849ac893c34ea7dfbd2258dc9c5
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-08-15 18:56:00 -0400

    shop card touch up

--- src/components/ShopCard/ShopCard.jsx
+++ src/components/ShopCard/ShopCard.jsx
@@ -7,16 +7,32 @@ function ShopCard ({Shop}) {
 <div class=" flex  flex-col  md:flex-row justify-center  flex-wrap gap-3 mt-10  ">
           <div class="pro-card">
         <div class="bg-white max-w-xs shadow-lg   mx-auto border-b-4 border-indigo-500 rounded-2xl overflow-hidden  hover:shadow-2xl transition duration-500 transform hover:scale-105 cursor-pointer" >
-        <div class="bg-indigo-500  flex h-20  items-center">
-        <h1 class="text-white ml-4 border-2 py-2 px-4 rounded-full">{Shop.name}</h1>
+        <div class="bg-indigo-500  flex h-200  items-center">
+          {/* add logo to top center  */}
+        <img src={Shop.logo} alt="shop's logo" />
         {/* <p class="ml-4 text-white uppercase">Title</p> */}
         </div>
+        {Shop.name &&
+          <h1 class="py-6 px-6 text-xl tracking-wide text-center">{Shop.name}</h1>
+        }
+        {Shop.address && 
           <p class="py-6 px-6 text-lg tracking-wide text-center">{Shop.address}</p>
+        }
+        {Shop.phoneNum &&
           <p class="py-6 px-6 text-lg tracking-wide text-center">{Shop.phoneNum}</p>
+        }
+        {Shop.email &&
           <p class="py-6 px-6 text-lg tracking-wide text-center">{Shop.email}</p>
+        }
+        {Shop.desc && 
           <p class="py-6 px-6 text-lg tracking-wide text-center">{Shop.desc}</p>
-          <p class="py-6 px-6 text-lg tracking-wide text-center">{Shop.tags}</p>
+        }
+        {Shop.tags && 
+          <p class="py-6 px-6 text-lg tracking-wide text-center">{Shop.tags.map(tag => <p>{tag}</p>)}</p>
+        }
+        {Shop.owner && 
           <p class="py-6 px-6 text-lg tracking-wide text-center">{Shop.owner.name}</p>
+        }
                                 {/* <!-- <hr > --> */}
 <Link to={`/shops/${Shop._id}`}>
             <div class="flex justify-center px-5 mb-2 text-sm ">
@@ -28,24 +44,6 @@ function ShopCard ({Shop}) {
         </div>
         </div>
         </div>
-
-    /* //   <Link to={`/shops/${Shop._id}`}>
-    //   <div className="card">
-    //   <div className="card-body">
-    //     <img src={Shop.logo} alt={`${Shop.name}'s Logo`}/>
-    //     <h2 className="card-text">{Shop.name}</h2>
-    //     <p className="card-text">{Shop.address}</p>
-    //     <p className="card-text">{Shop.phoneNum}</p>
-    //     <p className="card-text">{Shop.email}</p>
-    //     <p className="card-text">{Shop.desc}</p>
-    //     <p className="card-text">{Shop.tags}</p>
-    //     <p className="card-text">{Shop.owner.name}</p>
-    //   </div>
-    //   <div className="card-footer">
-       
-    //   </div>
-    // </div>
-    //   </Link> */
     )
 }
 
