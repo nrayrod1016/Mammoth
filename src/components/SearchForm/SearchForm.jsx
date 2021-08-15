e323d0eef295386794af7da298b8f98049b75efe
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-08-15 13:57:49 -0400

    navbar touch up

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -18,18 +18,23 @@ const SearchForm = (props) => {
 
   return (
     <>
-    <form onSubmit={handleSubmit}> 
-    <input
+    <div class="p-12">
+    <form  class="bg-white flex items-center rounded-full shadow-xl" onSubmit={handleSubmit}> 
+    <input 
+    class="rounded-l-full w-full py-4 px-7 text-gray-700 leading-tight focus:outline-none"
     type="text" 
     name="search"
     className="shopSearch"
     value={query} 
     onChange={handleChange}
     />  
-    <button>
+    <div>
+    <button class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center" >
         Search
     </button>
+    </div> 
     </form>
+    </div>
     </>
   );
 }
